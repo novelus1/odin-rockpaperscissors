d48a1862318f8cb9f7cfa7842a147af487b3ebc9
novelus1 <krzysztof.truszkowski@outlook.com>
2023-07-28 00:13:50 +0200

    PlayRound function change

--- my-app/src/RockPaperScissors.js
+++ my-app/src/RockPaperScissors.js
@@ -7,30 +7,6 @@ function getComputerChoice() {
     : "scissors";
 }
 
-function playRound(playerSelection, computerSelection) {
-  playerSelection = playerSelection.toLowerCase();
-  if (playerSelection === computerSelection) {
-    return "Tie!";
-  } else if (
-    (playerSelection === "rock" && computerSelection === "scissors") ||
-    (playerSelection === "paper" && computerSelection === "rock") ||
-    (playerSelection === "scissors" && computerSelection === "paper")
-  ) {
-    playerScore++;
-    return `You win, ${playerSelection} beats ${computerSelection}!`;
-  } else {
-    computerScore++;
-    return `You lose, ${computerSelection} beats ${playerSelection}!`;
-  }
-}
-
-const computerSelection = getComputerChoice();
-const playerSelection = "rock";
-
-console.log(playRound(playerSelection, computerSelection));
-
-console.log(computerSelection);
-
 function game() {
   let playerScore = 0;
   let computerScore = 0;
@@ -39,29 +15,27 @@ function game() {
 
   function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase();
-    return playerSelection === computerSelection
-      ? "Tie!"
-      : playerSelection === "rock" && computerSelection === "scissors"
-      ? (`You win, ${playerSelection} beats ${computerSelection}!`,
-        playerScore++)
-      : playerSelection === "paper" && computerSelection === "rock"
-      ? (`You win, ${playerSelection} beats ${computerSelection}!`,
-        playerScore++)
-      : playerSelection === "scissors" && computerSelection === "paper"
-      ? (`You win, ${playerSelection} beats ${computerSelection}!`,
-        playerScore++)
-      : (`You lose, ${computerSelection} beats ${playerSelection}!`,
-        computerScore++);
+    if (playerSelection === computerSelection) {
+      return "Tie!";
+    } else if (
+      (playerSelection === "rock" && computerSelection === "scissors") ||
+      (playerSelection === "paper" && computerSelection === "rock") ||
+      (playerSelection === "scissors" && computerSelection === "paper")
+    ) {
+      playerScore++;
+      return `You win, ${playerSelection} beats ${computerSelection}!`;
+    } else {
+      computerScore++;
+      return `You lose, ${computerSelection} beats ${playerSelection}!`;
+    }
   }
 
   while (round < roundsToPlay) {
     const computerSelection = getComputerChoice();
     const playerSelection = "rock"; // value to change
     const result = playRound(playerSelection, computerSelection);
+    console.log(playerSelection, computerSelection);
     console.log(result);
-    console.log(
-      `Player Score: ${playerScore}, Computer Score: ${computerScore}`
-    );
     round++;
   }
 
